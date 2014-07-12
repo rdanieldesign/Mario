3ce8fd2b9db1e3117796a0ef4c4350afb9cc8187
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2014-07-12 15:37:49 -0400

    Fix movement calculations
    Key movements now work with any size background object

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,29 +7,26 @@ $(document).keydown(function(e) {
 
     switch(e.which) {
         case 37: // left
-        if ((backgroundPosTop - backgroundHeight > 0) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight > 440) || (backgroundPosRight + backgroundWidth <= 440)){
-		$('#background').css( "right", backgroundPosRight - 40 + "px");};
+        if ((backgroundPosTop >= 240) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight > 440) || (backgroundPosRight + backgroundWidth <= 440)){
+		$('#background').css( 'right', backgroundPosRight - 40 + 'px');}
         break;
 
         case 38: // up
         if ((backgroundPosTop + backgroundHeight < 200) || (backgroundPosTop >= 240) || (backgroundPosRight >= 440) || (backgroundPosRight + backgroundWidth < 440)){
-		$('#background').css( "top", backgroundPosTop + 40 + "px");};
+		$('#background').css( 'top', backgroundPosTop + 40 + 'px');}
         break;
 
         case 39: // right
-		if ((backgroundPosTop - backgroundHeight > 0) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight + backgroundWidth <= 360) || (backgroundPosRight >= 440)){
-        $('#background').css( "right", backgroundPosRight + 40 + "px");};
+		if ((backgroundPosTop >= 240) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight + backgroundWidth <= 360) || (backgroundPosRight >= 440)){
+        $('#background').css( 'right', backgroundPosRight + 40 + 'px');}
         break;
 
         case 40: // down
         if ((backgroundPosTop > 240) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight >= 440) || (backgroundPosRight + backgroundWidth < 440)){
-		$('#background').css( "top", backgroundPosTop - 40 + "px");};
+		$('#background').css( 'top', backgroundPosTop - 40 + 'px');}
         break;
 
         default: return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
-
-var characterOffset = $('#character').offset();
-characterOffset.html( "left: " + characterOffset.left + ", top: " + characterOffset.top );
