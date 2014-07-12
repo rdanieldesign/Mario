246ae6afea86868a529a9c6e4b68e9ce452bd714
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2014-07-12 11:03:30 -0400

    Create boundaries around background object
    Use if statement to restrict movement of background object based on
    position and object size. Alter size to 40px  increments.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,35 @@
-$(document).ready(function(){
-	$('#move').click(function(){
-		var characterPosition = parseFloat($('#background').css("right"));
-		$('#background').css( "right", characterPosition + 100 + "px");
-	});
-});
-
 $(document).keydown(function(e) {
+    var backgroundPosRight = parseFloat($('#background').css('right'));
+    var backgroundPosTop = parseFloat($('#background').css('top'));
+    var backgroundHeight = parseFloat($('#background').css('height'));
+    var backgroundWidth = parseFloat($('#background').css('width'));
+
+
     switch(e.which) {
         case 37: // left
-        var characterPosition = parseFloat($('#background').css("right"));
-		$('#background').css( "right", characterPosition - 50 + "px");
+        if ((backgroundPosTop - backgroundHeight > 0) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight > 440) || (backgroundPosRight + backgroundWidth <= 440)){
+		$('#background').css( "right", backgroundPosRight - 40 + "px");};
         break;
 
         case 38: // up
-        var characterPosition = parseFloat($('#background').css("top"));
-		$('#background').css( "top", characterPosition + 50 + "px");
+        if ((backgroundPosTop + backgroundHeight < 200) || (backgroundPosTop >= 240) || (backgroundPosRight >= 440) || (backgroundPosRight + backgroundWidth < 440)){
+		$('#background').css( "top", backgroundPosTop + 40 + "px");};
         break;
 
         case 39: // right
-        var characterPosition = parseFloat($('#background').css("right"));
-		$('#background').css( "right", characterPosition + 50 + "px");
+		if ((backgroundPosTop - backgroundHeight > 0) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight + backgroundWidth <= 360) || (backgroundPosRight >= 440)){
+        $('#background').css( "right", backgroundPosRight + 40 + "px");};
         break;
 
         case 40: // down
-        var characterPosition = parseFloat($('#background').css("top"));
-		$('#background').css( "top", characterPosition - 50 + "px");
+        if ((backgroundPosTop > 240) || (backgroundPosTop + backgroundHeight <= 200) || (backgroundPosRight >= 440) || (backgroundPosRight + backgroundWidth < 440)){
+		$('#background').css( "top", backgroundPosTop - 40 + "px");};
         break;
 
         default: return; // exit this handler for other keys
     }
     e.preventDefault(); // prevent the default action (scroll / move caret)
 });
+
+var characterOffset = $('#character').offset();
+characterOffset.html( "left: " + characterOffset.left + ", top: " + characterOffset.top );
